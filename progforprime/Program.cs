12d7e458702f1d891714e2fdb37c172108c3aff8
Pallavikothawade <Pallavijkothawade@gmail.com>
2022-12-14 19:39:01 +0530

    Adding the batch236program

---
+++ progforprime/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace progforprime
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = 0, a = 0;
+            Console.WriteLine("Enter the number");
+            n = Convert.ToInt32(Console.ReadLine());
+            for (int i = 1; i <= n; i++)
+            {
+                if (n % i == 0)
+                {
+                    a++;
+                }
+
+            }
+            if (a == 2)
+            {
+                Console.WriteLine("This is a prime no");
+            }
+            else
+            {
+                Console.WriteLine("This is not prime no");
+            }
+            Console.ReadLine();
+        }
+    }
+}
